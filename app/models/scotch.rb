986c45cb933b1a9dbb65e5ead66b8c5201cb50fb
github username <github email address>
2016-12-15 16:50:53 -0500

    small change

--- app/models/scotch.rb
+++ app/models/scotch.rb
@@ -1,13 +1,13 @@
 class Scotch < ActiveRecord::Base
 	belongs_to :connoisseur
 	validates_presence_of :name, :rating, :price, :review
-	
+
 	def slug
 		name.downcase.gsub(" ","-")
 	end
 
 	def self.find_by_slug(slug)
-		Scotch.all.find { |scotch| scotch.slug == slug}
+		Scotch.all.find { |scotch| scotch.slug == slug }
 	end
 
 end
