26272c8933d8edf818fe8cc0c2dfe17b9099899a
github username <github email address>
2016-12-25 17:35:41 -0500

    fixed editing review

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -4,11 +4,13 @@ require 'rack-flash'
 class ReviewsController < ApplicationController
   use Rack::Flash
 
-  post '/review' do
+  post '/review' do #Create new review of scotch
     if logged_in?
       @review = current_user.reviews.new(params)
+      #@scotch = current_user.scotches.find_by(params[:id])
       if @review.valid? && @review.save
-        redirect back
+        #redirect back
+        redirect "/reviews/#{@review.scotch.slug}"
       else
         flash[:error] = "Must fill in all fields to complete review!"
         redirect back
@@ -17,11 +19,13 @@ class ReviewsController < ApplicationController
   end
 
   get "/review/:slug/edit" do
-    #@scotch = Scotch.find_by(params[:id])
-    @scotch = Scotch.find_by(params[:id])
-    @review = current_user.reviews.find_by(params[:id])
-    #@scotch = Scotch.find_by(params[:id])
     if logged_in?
+      @review = current_user.reviews.find_by(params[:id])
+      #@review = current_user.reviews.find_by(review_id)
+      reviews = params[:reviews_id]
+      @reviews = params[:reviews_id]
+      @reviewer = Review.find_by(params[:reviews])
+      #@review_id = @reviews.id
       if @review.connoisseur_id == session[:id]
         erb :"/reviews/edit_review"
       elsif @review.connoisseur_id != session[:id]
@@ -35,16 +39,18 @@ class ReviewsController < ApplicationController
 
   patch "/review/:slug" do
     if logged_in?
-      #@scotch = Scotch.find_by(params[:id])
-      @reviewer = Review.find_by(params[:scotch_id])
-      @scotch = Scotch.find_by(params[:id])
-      @review = current_user.reviews.find_by(params[:id])
-      if @review.connoisseur_id == session[:id]
-        @review.rating = params[:rating]
-        @review.description = params[:description]
-        @review.save
-        redirect "/reviews/#{@review.scotch.slug}"
-      elsif @reviewer.connoisseur_id != session[:id]
+      #reviews = params[:reviews_id]
+      #@reviews = params[:reviews_id]
+      @reviewers = Review.find_by_id(params[:reviews_id])
+      @review = Review.find_by(params[:@reviews])
+      if @reviewers.connoisseur_id == session[:id]
+        @reviewers.rating = params[:rating]
+        @reviewers.description = params[:description]
+        @reviewers.save
+        #@review.update_attributes(params)
+        redirect "/reviews/#{@reviewers.scotch.slug}"
+        #redirect "/reviews/#{@review.scotch.slug}"
+      elsif @reviewers.connoisseur_id != session[:id]
         flash.now[:error] = "You do not have write privileges to this review! Head back to reviews!"
         redirect "/reviews"
       else
--- app/controllers/scotches_controller.rb
+++ app/controllers/scotches_controller.rb
@@ -8,7 +8,7 @@ class ScotchesController < ApplicationController
 		@title = "Reviews"
 		if logged_in?
 			@scotch = Scotch.all
-			@connoisseur = Connoisseur.find(session[:id])
+#			@connoisseur = Connoisseur.find(session[:id])
 			erb :"/scotches/reviews"
 		else
 			redirect '/login'
@@ -48,64 +48,64 @@ class ScotchesController < ApplicationController
 		end
 	end
 
-	get "/reviews/:slug/edit" do
-		@title = "Edit Review"
-		@scotch = Scotch.find_by_slug(params[:slug])
-		if logged_in?
-			if @scotch.connoisseur_id == session[:id]
-				erb :"/scotches/edit_review"
-			elsif @scotch.connoisseur_id != session[:id]
-				flash.now[:error] = "You do not have write privileges to this review! Head back to reviews"
-				erb :"/scotches/edit_review"
-			else
-				redirect "/reviews"
-			end
-		else
-			"/login"
-		end
-	end
+	#get "/reviews/:slug/edit" do
+	#	@title = "Edit Review"
+	#	@scotch = Scotch.find_by_slug(params[:slug])
+	#	if logged_in?
+	#		if @scotch.connoisseur_id == session[:id]
+	#			erb :"/scotches/edit_review"
+	#		elsif @scotch.connoisseur_id != session[:id]
+	#			flash.now[:error] = "You do not have write privileges to this review! Head back to reviews"
+	#			erb :"/scotches/edit_review"
+	#		else
+	#			redirect "/reviews"
+	#		end
+	#	else
+	#		"/login"
+	#	end
+	#end
 
-	patch "/reviews/:slug" do
-		if logged_in?
-			@scotch = Scotch.find_by_slug(params[:slug])
-			if @scotch.connoisseurs_id != session[:id]
+	#patch "/reviews/:slug" do
+	#	if logged_in?
+	#		@scotch = Scotch.find_by_slug(params[:slug])
+	#		if @scotch.connoisseurs_id != session[:id]
+#
+	#			#needs to use flash.now so that the error message doesn't show up instantly
+	#			flash.now[:error] = "You do not have write privileges to this review! Head back to reviews"
+	#			erb :"/scotches/edit_review"
+#
+	#		elsif @scotch.connoisseurs_id == session[:id] && params[:name] == "" || params[:rating] == "" || params[:price] == "" || params[:review] == ""
+#
+#
+	#			#needs to use flash.now so that the error message doesn't show up instantly
+	#			flash.now[:error] = "Must fill in all fields to complete review!"
+	#			erb :"/scotches/edit_review"
+#
+	#		elsif @scotch.connoisseurs_id == session[:id]
+	#			@scotch.name = params[:name]
+	#			@scotch.rating = params[:rating]
+	#			@scotch.price = params[:price]
+	#			@scotch.review = params[:review]
+	#			@scotch.save
+	#			redirect '/reviews'
+	#		else
+	#			redirect '/reviews'
+	#		end
+	#	end
+	#end
 
-				#needs to use flash.now so that the error message doesn't show up instantly
-				flash.now[:error] = "You do not have write privileges to this review! Head back to reviews"
-				erb :"/scotches/edit_review"
-
-			elsif @scotch.connoisseurs_id == session[:id] && params[:name] == "" || params[:rating] == "" || params[:price] == "" || params[:review] == ""
-
-
-				#needs to use flash.now so that the error message doesn't show up instantly
-				flash.now[:error] = "Must fill in all fields to complete review!"
-				erb :"/scotches/edit_review"
-
-			elsif @scotch.connoisseurs_id == session[:id]
-				@scotch.name = params[:name]
-				@scotch.rating = params[:rating]
-				@scotch.price = params[:price]
-				@scotch.review = params[:review]
-				@scotch.save
-				redirect '/reviews'
-			else
-				redirect '/reviews'
-			end
-		end
-	end
-
-	delete "/reviews/:slug/delete" do
-		if logged_in?
-			@scotch = Scotch.find_by_slug(params[:slug])
-			if @scotch.connoisseur_id == session[:id]
-				@scotch.delete
-				redirect '/reviews'
-			else
-				redirect '/reviews'
-			end
-		else
-			redirect '/login'
-		end
-	end
+	#delete "/reviews/:slug/delete" do
+	#	if logged_in?
+	#		@scotch = Scotch.find_by_slug(params[:slug])
+	#		if @scotch.connoisseur_id == session[:id]
+	#			@scotch.delete
+	#			redirect '/reviews'
+	#		else
+	#			redirect '/reviews'
+	#		end
+	#	else
+	#		redirect '/login'
+	#	end
+	#end
 
 end
